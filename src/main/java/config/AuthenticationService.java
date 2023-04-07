464093d7b1dee30f770fbcf15429007f38c456af
khalil el kadih <khalil94elkadih@gmail.com>
2023-04-07 23:14:18 +0000

    Added spring Security to project

---
+++ src/main/java/config/AuthenticationService.java
@@ -0,0 +1,44 @@
+package config;
+import lombok.var;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
+
+
+
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtils jwtUtils, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public ResponseDTO register(RegisterDTO request) {
+        Users user = Users.builder()
+                .fname(request.getFirstName())
+                .lname(request.getLastName())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.CLIENT)
+                .email(request.getEmail())
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtUtils.generateToken(user);
+        return ResponseDTO.builder().token(jwtToken).build();
+    }
+
+    public ResponseDTO authenticate(ResponseDTO responseDTO) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(responseDTO.getEmail(), responseDTO.getPassword()));
+        Users user = userRepository.findByEmail(responseDTO.getEmail()).get();
+        String jwtToken = jwtUtils.generateToken(user);
+
+        return ResponseDTO.builder().token(jwtToken).build();
+    }
+}
